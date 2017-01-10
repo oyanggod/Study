b9a96985b48a29de3d0b8e1a752d474813e3a54b
oyanggod <oyangtianxia@126.com>
2017-01-10 12:01:38 +0800

    项目初次提交

---
+++ src/main/java/com/oy/action/TestAction.java
@@ -0,0 +1,16 @@
+package com.oy.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/Hello")
+public class TestAction {
+
+	@RequestMapping(value = "/World")
+	public String sayHelloWorld() {
+		System.out.println("Hello World");
+		return "hello";
+	}
+
+}
